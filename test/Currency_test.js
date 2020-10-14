644374ecc274113d26f16a26a789cd797f885917
Weiran Fu <fuweiran816@gmail.com>
2020-10-14 12:37:49 -0400

    update tests for Currency

--- test/Currency_test.js
+++ test/Currency_test.js
@@ -5,23 +5,27 @@ const Currency = require("../code/conversions/Currency");
 
 describe("Currency", function () {
     let usd = new Currency("USD", [
+        "usd",
         "eur",
         "gbp",
         "inr",
         "jpy",
         "cad",
         "aud",
-        "chf"
+        "chf",
+        "cny"
     ]);
     it("Conversions should not be empty", function () {
-        assert.notStrictEqual(usd.getAllConversions(30.99, 2), "");
+        let res = usd.getAllConversions(30.99, 2);
+        assert.notStrictEqual(res, "");
     });
 
     it("Standard Conversions should not be empty", function () {
-        assert.notStrictEqual(usd.getStandardConversion(30), "");
+        let res = usd.getStandardConversion(30);
+        assert.notStrictEqual(res, 0);
     });
 
-    let eur = new Currency("EUR", ["usd", "gbp", "inr", "jpy", "cad", "aud", "chf"]);
+    let eur = new Currency("EUR", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in EUR", function () {
         assert.notStrictEqual(eur.getAllConversions(49.94, 2), "");
     }
@@ -31,7 +35,7 @@ describe("Currency", function () {
     }
     );
 
-    let gbp = new Currency("GBP", ["usd", "eur", "inr", "jpy", "cad", "aud", "chf"]);
+    let gbp = new Currency("GBP", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in GBP", function () {
         assert.notStrictEqual(gbp.getAllConversions(49.94, 2), "");
     }
@@ -41,7 +45,7 @@ describe("Currency", function () {
     }
     );
 
-    let inr = new Currency("INR", ["usd", "eur", "gbp", "jpy", "cad", "aud", "chf"]);
+    let inr = new Currency("INR", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in INR", function () {
         assert.notStrictEqual(inr.getAllConversions(49.94, 2), "");
     }
@@ -51,7 +55,7 @@ describe("Currency", function () {
     }
     );
 
-    let jpy = new Currency("JPY", ["usd", "eur", "gbp", "inr", "cad", "aud", "chf"]);
+    let jpy = new Currency("JPY", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in JPY", function () {
         assert.notStrictEqual(jpy.getAllConversions(49.94, 2), "");
     }
@@ -61,7 +65,7 @@ describe("Currency", function () {
     }
     );
 
-    let cad = new Currency("JPY", ["usd", "eur", "gbp", "inr", "jpy", "aud", "chf"]);
+    let cad = new Currency("CAD", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in CAD", function () {
         assert.notStrictEqual(cad.getAllConversions(49.94, 2), "");
     }
@@ -71,7 +75,7 @@ describe("Currency", function () {
     }
     );
 
-    let aud = new Currency("JPY", ["usd", "eur", "gbp", "inr", "jpy", "cad", "chf"]);
+    let aud = new Currency("AUD", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in AUD", function () {
         assert.notStrictEqual(aud.getAllConversions(49.94, 2), "");
     }
@@ -81,7 +85,7 @@ describe("Currency", function () {
     }
     );
 
-    let chf = new Currency("JPY", ["usd", "eur", "gbp", "inr", "jpy", "cad", "aud"]);
+    let chf = new Currency("CHF", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
     it("Conversions in CHF", function () {
         assert.notStrictEqual(chf.getAllConversions(49.94, 2), "");
     }
@@ -91,6 +95,14 @@ describe("Currency", function () {
     }
     );
 
-
+    let cny = new Currency("CNY", ["usd", "eur", "gbp", "inr", "jpy", "cad","aud","chf", "cny"]);
+    it("Conversions in CNY", function () {
+        assert.notStrictEqual(cny.getAllConversions(49.94, 2), "");
+    }
+    );
+    it("Standard Conversions in CNY", function () {
+        assert.notStrictEqual(cny.getStandardConversion(30), "");
+    }
+    );
 
 });
