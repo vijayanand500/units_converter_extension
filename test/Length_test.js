8806a930e63259f8e3b1bcb1f33f39019a1a234a
Weiran Fu <fuweiran816@gmail.com>
2020-10-14 13:35:06 -0400

    add more tests for Length class

--- test/Length_test.js
+++ test/Length_test.js
@@ -8,14 +8,54 @@ describe('Length', function() {
       const length = new Length("kilometers", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
       assert.strictEqual(length.getStandardConversion(3), 3000)
     })
+    it('should not be empty', function() {
+      const length = new Length("meters", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("centimeters", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("millimeters", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("yard", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("feet", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("inch", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("furlong", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("chain", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("miles", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
+    it('should not be empty', function() {
+      const length = new Length("", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.notStrictEqual(length.getStandardConversion(3), 0)
+    })
   })
 })
 
 describe('Length', function() {
   describe('#getAllConversions(quantity, precision)', function() {
     it('should return ', function() {
-      const length = new Length("kilometers", ["meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
-      assert.strictEqual(length.getAllConversions(3, 2), ',3.0 m,0.001864512119 mi,300 cm,3000 mm,3.28083 yd,9.84252 ft,118.1103 inch,0.01492537313 fur,0.1491276035 chain')
+      const length = new Length("kilometers", ["kilometers", "meters", "miles", "centimeters", "millimeters", "yard", "feet", "inch", "furlong", "chain"])
+      assert.strictEqual(length.getAllConversions(3, 2), ',0.003000 km,3.0 m,0.001864512119 mi,300 cm,3000 mm,3.28083 yd,9.84252 ft,118.1103 inch,0.01492537313 fur,0.1491276035 chain')
     })
   })
 })
