d61d15ae27c18b7a5716f6e235285702d2d7aaa9
TianQ20 <tianqiangcao@gmail.com>
2020-10-28 01:54:54 -0400

    add comment for Utilities documentation

--- code/main_scripts/utility.js
+++ code/main_scripts/utility.js
@@ -1,28 +1,51 @@
-/*
+/**
     The following file has all the utility functions that perform trivial yet important tasks
 */
 
-//This is a sort function used to sort an array of string in descending order of string lengths
+/**
+ * This is a sort function used to sort an array of string in descending order of string lengths
+ * @function descending_length
+ * @param {Array} a
+ * @param {Array} b
+ * @returns sort array in decending order
+ */
 function descending_length(a, b) {
     return b.length - a.length;
 }
 
 
-//This is a filtering function used by  get_possible_conversions_list to filter out the current unit itself
-function matches_filter(currentUnit) {
+/**
+ * This is a filtering function used by get_possible_conversions_list to filter out the current unit itself
+ * @function matches_filter
+ * @param {String} currentUnit
+ * @returns return true if currentUnit lower case is different with element lower case, else return false
+*/
+ function matches_filter(currentUnit) {
     return function (element) {
         return element.toLowerCase() != currentUnit.toLowerCase();
     }
 }
 
-//For a given unit, this function returns all the popular units that it can be converted into except itself
-function get_possible_conversions_list(type, currentUnit) {
+/**
+ * For a given unit, this function returns all the popular units that it can be converted into except itself
+ * @function get_possible_conversions_list
+ * @param {String} type
+ * @param {String} currentUnit
+ * @returns all the popular units that it can be converted into except itself
+*/
+ function get_possible_conversions_list(type, currentUnit) {
     return POPULAR_UNITS[type].filter(matches_filter(currentUnit))
 }
 
 
-//Return appropriate conversion class from the conversion scripts based on unit type
-function get_conversion_class(type, unit) {
+/**
+ * Return appropriate conversion class from the conversion scripts based on unit type
+ * @function get_conversion_class
+ * @param {String} type
+ * @param {String} unit
+ * @returns appropriate conversion class
+*/
+ function get_conversion_class(type, unit) {
     if (type == "temperature") {
         return new Temperature(unit, get_possible_conversions_list(type, unit));
     }
@@ -50,8 +73,13 @@ function get_conversion_class(type, unit) {
 
 }
 
-//Get a number's precision
-function getPrecision(number) {
+/**
+ * Get a number's precision
+ * @function getPrecision
+ * @param {Number} number
+ * @returns number's precision
+*/
+ function getPrecision(number) {
     let parts = number.toString().split(".");
     if (parts.length <= 1) {
         return number < 0 ? parts[0].length - 1 : parts[0].length;
@@ -60,7 +88,13 @@ function getPrecision(number) {
     return intlen + parts[1].length;
 }
 
-//Return number with a max precison of 10
-function getPreciseNumber(number, precision) {
+/**
+ * Return number with a max precison of 10
+ * @function getPreciseNumber
+ * @param {Number} number
+ * @param {Number} precision
+ * @returns number with a max precison of 10
+*/
+ function getPreciseNumber(number, precision) {
     return number.toPrecision(Math.min(Math.max(getPrecision(number), precision), 10))
 }
